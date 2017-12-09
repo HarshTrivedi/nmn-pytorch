210a71e9451d67b331d83b2e95b2ffef3b269245
harshtrivedi <harshjtrivedi94@gmail.com>
2017-12-09 10:51:18 -0500

    reorganize training files to be handled standalone

--- train_baseline_lstm_img.py
+++ train_baseline_lstm_img.py
@@ -1,6 +1,5 @@
 from baseline_lstm_img_model import *
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 from lib.data_loader import *
 from torch.autograd import Variable
 import torch.nn.functional as F
@@ -10,53 +9,6 @@ import json
 import os
 import time
 import logging
-
-
-######## Set up (Starts) #########
-
-model_name   = 'baseline_lstm_img'
-train_set = 'train2014-sub'
-val_set  = 'val2014-sub'
-
-root_dir = os.path.dirname(os.path.realpath(__file__))
-log_dir = os.path.join(root_dir, 'logs')
-logging.basicConfig(    filename= os.path.join(log_dir, model_name + '.log'), 
-                        level=logging.INFO, 
-                        format="%(asctime)s:%(message)s", 
-                        filemode='w' )
-
-results_dir = os.path.join( root_dir, 'results' )
-train_results_file = os.path.join( results_dir, model_name + '.json' )
-val_results_file  = os.path.join( results_dir, model_name + '.json' )
-
-if not os.path.exists(results_dir):
-    os.makedirs(results_dir)
-
-saved_mean_stds_file = os.path.join(root_dir, 
-                    'preprocessed_data/image_features/mean-stds-{}.pickle'.format(train_set) )
-
-annotation_file = os.path.join( root_dir, 
-            'raw_data/Annotations/v2_mscoco_%s_annotations.json' )
-
-vocab_answer_file = os.path.join( root_dir,
-            'preprocessed_data/vqa_answers.txt')
-
-
-save_models_dir = os.path.join(root_dir, 'saved_models')
-save_model_file = os.path.join(save_models_dir, model_name + '.pt')
-if not os.path.exists(save_models_dir):
-    os.makedirs(save_models_dir)
-
-
-use_gpu = torch.cuda.is_available()
-logging.info("GPU availibility: {}".format(use_gpu))
-
-if use_gpu:
-    torch.cuda.manual_seed(1)
-else:
-    torch.manual_seed(1)    
-
-######## Set up (Ends) #########
 
 
 
@@ -160,11 +112,13 @@ def test_model(model, datasetloader, dataset):
             actual_label_tensor = Variable(batch_datums['answer_index'].cuda(), requires_grad = False).view(-1)
             token_sequence_tensor = Variable(batch_datums['question_token_ids'].cuda(), requires_grad = False)
             answer_vector_tensor  = Variable(batch_datums['answer_vector_tensor'].cuda(), requires_grad = False)
+            question_id = Variable(batch_datums['question_id'].cuda(), requires_grad = False)
         else:
             image_feat = Variable(batch_datums['image_feature_tensor'], requires_grad = False)
             actual_label_tensor = Variable(batch_datums['answer_index'], requires_grad = False).view(-1)
             token_sequence_tensor = Variable(batch_datums['question_token_ids'], requires_grad = False)            
             answer_vector_tensor  = Variable(batch_datums['answer_vector_tensor'], requires_grad = False)
+            question_id = Variable(batch_datums['question_id'], requires_grad = False)
         prediction_scores_tensor = model.forward(image_feat, token_sequence_tensor )
 
 
@@ -247,66 +201,116 @@ def visualize_model(model, datasetloader, dataset, num_questions=10):
 
 ###################################################################################
 
-train_dataset, train_mean_stds  = get_dataset(train_set, None, saved_mean_stds_file )
-val_dataset, _                  = get_dataset(val_set, train_mean_stds)
-logging.info("Datasets Loaded")
 
-dataset = { 'train': train_dataset, 'val'  : val_dataset }
+if __name__ == "__main__":
 
-train_annotation_dict = json.load(open(annotation_file % train_set))
-val_annotation_dict   = json.load(open(annotation_file % val_set))
+    ######## Set up (Starts) #########
 
-D_img, D_txt, D_map, D_hidden = 512, 300, 1024, 1024
-with open(vocab_answer_file, 'r') as f:
-    D_ans_choices = len( [e for e in f.readlines() if e.strip() != ""] )
+    model_name   = 'baseline_lstm_img'
+    train_set = 'train2014-sub'
+    val_set  = 'val2014-sub'
 
-question_word_embeddings_np = dataset['train'].question_embedding_matrix
-question_vocab_size         = dataset['train'].question_vocab_dict.num_vocab
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    log_dir = os.path.join(root_dir, 'logs')
+    logging.basicConfig(    filename= os.path.join(log_dir, model_name + '.log'), 
+                            level=logging.INFO, 
+                            format="%(asctime)s:%(message)s", 
+                            filemode='w' )
 
-######
-# change this block Model Wise
-model = VGABaseModel(   D_img, 
-                        D_txt, 
-                        D_map, 
-                        D_hidden, 
-                        question_word_embeddings_np, 
-                        question_vocab_size,  
-                        D_ans_choices)
-######
+    results_dir = os.path.join( root_dir, 'results' )
+    train_results_file = os.path.join( results_dir, '_'.join([ model_name, train_set, train_set ]) + '.json' )
+    val_results_file  = os.path.join( results_dir, '_'.join([ model_name, train_set, val_set ]) + '.json' )
 
-if use_gpu:
-    model.cuda()
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
 
-loss_function = torch.nn.CrossEntropyLoss()
-parameters = filter(lambda p: p.requires_grad, model.parameters())
-optimizer = torch.optim.Adadelta(parameters)
-logging.info('Built Model')
+    saved_mean_stds_file = os.path.join(root_dir, 
+                        'preprocessed_data/image_features/mean-stds-{}.pickle'.format(train_set) )
 
-train_dataloader = DataLoader(dataset['train'], batch_size=1, shuffle=True, num_workers=4)
-val_dataloader   = DataLoader(dataset['val'] ,  batch_size=1, shuffle=True, num_workers=4)
-dataloader = { 'train': train_dataloader, 'val' : val_dataloader }
-logging.info('Dataloader Loaded')
+    annotation_file = os.path.join( root_dir, 
+                'raw_data/Annotations/v2_mscoco_%s_annotations.json' )
 
-trained_vqa_model = train_model(model, dataloader, dataset, loss_function, optimizer, num_epochs=50)
-logging.info('Training Complete')
-
-torch.save(trained_vqa_model, save_model_file)
-logging.info('Saved Model')
+    vocab_answer_file = os.path.join( root_dir,
+                'preprocessed_data/vqa_answers.txt')
 
 
-train_result_dict, train_accuracy = test_model(trained_vqa_model, train_dataloader, train_dataset)
-val_result_dict  , val_accuracy   = test_model(trained_vqa_model, val_dataloader , val_dataset)
+    save_models_dir = os.path.join(root_dir, 'saved_models')
+    save_model_file = os.path.join(save_models_dir, model_name + '.pt')
+    if not os.path.exists(save_models_dir):
+        os.makedirs(save_models_dir)
 
-with open(train_results_file, 'w') as f:
-    json.dump(train_result_dict, f)
 
-with open(val_results_file, 'w') as f:
-    json.dump(val_result_dict, f)
+    use_gpu = torch.cuda.is_available()
+    logging.info("GPU availibility: {}".format(use_gpu))
 
-logging.info('saved result jsons')
+    if use_gpu:
+        torch.cuda.manual_seed(1)
+    else:
+        torch.manual_seed(1)    
 
-# MatplotLib would pop to show
-visualize_model(trained_vqa_model, val_dataloader, val_dataset, num_questions=10)
+    ######## Set up (Ends) #########
+
+
+    train_dataset, train_mean_stds  = get_dataset(train_set, None, saved_mean_stds_file )
+    val_dataset, _                  = get_dataset(val_set, train_mean_stds)
+    logging.info("Datasets Loaded")
+
+    dataset = { 'train': train_dataset, 'val'  : val_dataset }
+
+    train_annotation_dict = json.load(open(annotation_file % train_set))
+    val_annotation_dict   = json.load(open(annotation_file % val_set))
+
+    D_img, D_txt, D_map, D_hidden = 512, 300, 1024, 1024
+    with open(vocab_answer_file, 'r') as f:
+        D_ans_choices = len( [e for e in f.readlines() if e.strip() != ""] )
+
+    question_word_embeddings_np = dataset['train'].question_embedding_matrix
+    question_vocab_size         = dataset['train'].question_vocab_dict.num_vocab
+
+    ######
+    # change this block Model Wise
+    model = VGABaseModel(   D_img, 
+                            D_txt, 
+                            D_map, 
+                            D_hidden, 
+                            question_word_embeddings_np, 
+                            question_vocab_size,  
+                            D_ans_choices)
+    ######
+
+    if use_gpu:
+        model.cuda()
+
+    loss_function = torch.nn.CrossEntropyLoss()
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = torch.optim.Adadelta(parameters)
+    logging.info('Built Model')
+
+    train_dataloader = DataLoader(dataset['train'], batch_size=1, shuffle=True, num_workers=4)
+    val_dataloader   = DataLoader(dataset['val'] ,  batch_size=1, shuffle=True, num_workers=4)
+    dataloader = { 'train': train_dataloader, 'val' : val_dataloader }
+    logging.info('Dataloader Loaded')
+
+    trained_vqa_model = train_model(model, dataloader, dataset, loss_function, optimizer, num_epochs=50)
+    logging.info('Training Complete')
+
+    torch.save(trained_vqa_model, save_model_file)
+    logging.info('Saved Model')
+
+
+    train_result_dict, train_accuracy = test_model(trained_vqa_model, train_dataloader, train_dataset)
+    val_result_dict  , val_accuracy   = test_model(trained_vqa_model, val_dataloader , val_dataset)
+
+    with open(train_results_file, 'w') as f:
+        json.dump(train_result_dict, f)
+
+    with open(val_results_file, 'w') as f:
+        json.dump(val_result_dict, f)
+
+    logging.info('saved result jsons')
+
+    # MatplotLib would pop to show
+    visualize_model(trained_vqa_model, val_dataloader, val_dataset, num_questions=10)
 
 
 
